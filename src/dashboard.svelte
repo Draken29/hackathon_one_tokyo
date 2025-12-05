0a9db9449d2e2eb10227653e558c547c797c1cdc
mariamKamal809 <mariamkamal6145@gmail.com>
2025-12-05 17:58:02 +0300

    Dashboard

---
+++ src/dashboard.svelte
@@ -0,0 +1,135 @@
+<script>
+</script>
+
+
+
+<main>
+  <nav class="navbar fixed top-0 left-0 w-full z-50 bg-black/20 backdrop-blur-md text-white px-10 py-4 flex justify-between items-center border-b-2 border-gray-400">
+    <div class="text-2xl font-bold">Project</div>
+
+    <ul class="flex gap-8 text-lg">
+      <li><a href="#home" class="hover:text-gray-300">Upload Image</a></li>
+      <li><a href="#home" class="hover:text-gray-300">Dashboard</a></li>
+      <li><a href="#home" class="hover:text-gray-300">logout</a></li>
+
+  </ul>
+  </nav>
+  <div class="flex flex-col justify-center items-center gap-3 mt-30">
+    <div class="porfile flex justify-center items-center bg-white w-250 p-10">
+      <div class="flex-1 flex flex-col justify-center items-center gap-5 w-120">
+        <img class="userimage w-40 h-40 rounded-full object-cover overflow-hidden" src="/src/assets/young-bearded-man-with-striped-shirt_273609-5677.avif" alt="usreImage">
+        <div class="flex flex-row justify-center items-center gap-5">
+          <!-- svelte-ignore a11y_consider_explicit_label -->
+          <button class="flex flex-col justify-center items-center bg-white p-3 rounded-xl">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-image-up-icon lucide-image-up"><path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21"/><path d="m14 19.5 3-3 3 3"/><path d="M17 22v-5.5"/><circle cx="9" cy="9" r="2"/></svg>
+          </button>
+          <!-- svelte-ignore a11y_consider_explicit_label -->
+          <button class="flex flex-col justify-center items-center bg-white p-3 rounded-xl">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-camera-icon lucide-camera"><path d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z"/><circle cx="12" cy="13" r="3"/></svg>
+          </button>
+        </div>
+      </div>
+      <div class="flex-3 px-10">
+        <div class="text-3xl text-white font-bold">Name: <span>Mustafa Kamal</span></div>
+        <div class="text-3xl text-white font-bold">Email: <span>user@gmail.com</span></div>
+        <div class="text-3xl text-white font-bold">created in: 2/12/2025</div>
+      </div>
+
+    </div>
+
+    <div class="text-5xl text-white font-bold mb-7">History</div>
+
+    <div class="flex justify-start items-center gap-5 w-250 flex-wrap ">
+
+      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+        <div class="p-3 flex items-center gap-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+          <div>
+            <p class="text-xl text-white font-bold pt-1">image 1</p>
+            <p class="text-l text-white">date: 5/12/2025</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+        <div class="p-3 flex items-center gap-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+          <div>
+            <p class="text-xl text-white font-bold pt-1">image 1</p>
+            <p class="text-l text-white">date: 5/12/2025</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+        <div class="p-3 flex items-center gap-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+          <div>
+            <p class="text-xl text-white font-bold pt-1">image 1</p>
+            <p class="text-l text-white">date: 5/12/2025</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+        <div class="p-3 flex items-center gap-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+          <div>
+            <p class="text-xl text-white font-bold pt-1">image 1</p>
+            <p class="text-l text-white">date: 5/12/2025</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="border border-gray-500 w-80 rounded-3xl overflow-hidden flex flex-col">
+        <img class="object-cover" src="/src/assets/t3qWG.png" alt="textimage">
+        <div class="p-3 flex items-center gap-5">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text bg-purple-600 p-1 rounded-md"><path d="M6 22a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.704.706l3.588 3.588A2.4 2.4 0 0 1 20 8v12a2 2 0 0 1-2 2z"/><path d="M14 2v5a1 1 0 0 0 1 1h5"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>
+          <div>
+            <p class="text-xl text-white font-bold pt-1">image 1</p>
+            <p class="text-l text-white">date: 5/12/2025</p>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  </div>
+</main>
+
+<style>
+  .navbar {
+    background: #262626
+  }
+  .porfile{
+    background: #262626; /* أغمق قليلاً من الخلفية */
+    border: 1px solid rgba(255, 255, 255, 0.85);
+    border-radius: 16px;
+    padding: 24px;
+    box-shadow:
+      0 0 0 1px rgba(255, 255, 255, 0.35) inset, /* خط داخلي خفيف */
+      0 0 5px rgba(255, 255, 255, 0.15),       /* توهج قريب */
+      0 0 10px rgba(255, 255, 255, 0.12);       /* توهج أبعد */
+    backdrop-filter: blur(2px);
+  }
+  .historyCard {
+    box-shadow:
+      0 0 0 1px rgba(255, 255, 255, 0.55) inset, /* خط داخلي خفيف */
+      0 0 10px rgba(255, 255, 255, 0.35),       /* توهج قريب */
+      0 0 24px rgba(255, 255, 255, 0.22);       /* توهج أبعد */
+    backdrop-filter: blur(2px);
+  }
+  .userimage{
+    border: 1px solid rgba(255, 255, 255, 0.85);
+    box-shadow:
+      0 0 0 1px rgba(255, 255, 255, 0.55) inset, /* خط داخلي خفيف */
+      0 0 10px rgba(255, 255, 255, 0.35),       /* توهج قريب */
+      0 0 24px rgba(255, 255, 255, 0.22);       /* توهج أبعد */
+    backdrop-filter: blur(2px);
+  }
+</style>
